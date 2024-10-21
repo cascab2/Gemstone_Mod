38b563d0a6b4cff10ea92edcda512fa9faad0503
caharter6440 <1@2.3>
2024-10-21 09:58:11 -0700

    a

--- src/main/java/com/caleb/gemstonemod/worldgen/ModConfiguredFeatures.java
+++ src/main/java/com/caleb/gemstonemod/worldgen/ModConfiguredFeatures.java
@@ -24,22 +24,24 @@ public class ModConfiguredFeatures {
     public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_OPALITE_ORE_KEY = registerKey("opalite_ore");
 
     public static void bootstrap(BootstrapContext<ConfiguredFeature<?, ?>> context) {
-        RuleTest stoneReplaceable = new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES);
-        RuleTest deepslateReplaceable = new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES);
+        RuleTest stoneReplaceables = new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES);
+        RuleTest deepslateReplaceables = new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES);
 
-        List<OreConfiguration.TargetBlockState> amberiteOres = List.of(OreConfiguration.target(stoneReplaceable,
+        List<OreConfiguration.TargetBlockState> overworldAmberiteOres = List.of(OreConfiguration.target(stoneReplaceables,
                 ModBlocks.AMBERITE_ORE.get().defaultBlockState()),
-                OreConfiguration.target(deepslateReplaceable, ModBlocks.DEEPSLATE_AMBERITE_ORE.get().defaultBlockState()));
-        List<OreConfiguration.TargetBlockState> saphiriteOres = List.of(OreConfiguration.target(stoneReplaceable,
-                        ModBlocks.SAPHIRITE_ORE.get().defaultBlockState()),
-                OreConfiguration.target(deepslateReplaceable, ModBlocks.DEEPSLATE_SAPHIRITE_ORE.get().defaultBlockState()));
-        List<OreConfiguration.TargetBlockState> opaliteOres = List.of(OreConfiguration.target(stoneReplaceable,
-                        ModBlocks.OPALITE_ORE.get().defaultBlockState()),
-                OreConfiguration.target(deepslateReplaceable, ModBlocks.DEEPSLATE_OPALITE_ORE.get().defaultBlockState()));
+                OreConfiguration.target(deepslateReplaceables, ModBlocks.DEEPSLATE_AMBERITE_ORE.get().defaultBlockState()));
 
-        register(context, OVERWORLD_AMBERITE_ORE_KEY, Feature.ORE, new OreConfiguration(amberiteOres, 2));
-        register(context, OVERWORLD_SAPHIRITE_ORE_KEY, Feature.ORE, new OreConfiguration(saphiriteOres, 2));
-        register(context, OVERWORLD_OPALITE_ORE_KEY, Feature.ORE, new OreConfiguration(opaliteOres, 2));
+        List<OreConfiguration.TargetBlockState> overworldSaphiriteOres = List.of(OreConfiguration.target(stoneReplaceables,
+                        ModBlocks.SAPHIRITE_ORE.get().defaultBlockState()),
+                OreConfiguration.target(deepslateReplaceables, ModBlocks.DEEPSLATE_SAPHIRITE_ORE.get().defaultBlockState()));
+
+        List<OreConfiguration.TargetBlockState> overworldOpaliteOres = List.of(OreConfiguration.target(stoneReplaceables,
+                        ModBlocks.OPALITE_ORE.get().defaultBlockState()),
+                OreConfiguration.target(deepslateReplaceables, ModBlocks.DEEPSLATE_OPALITE_ORE.get().defaultBlockState()));
+
+        register(context, OVERWORLD_AMBERITE_ORE_KEY, Feature.ORE, new OreConfiguration(overworldAmberiteOres, 2));
+        register(context, OVERWORLD_SAPHIRITE_ORE_KEY, Feature.ORE, new OreConfiguration(overworldSaphiriteOres, 2));
+        register(context, OVERWORLD_OPALITE_ORE_KEY, Feature.ORE, new OreConfiguration(overworldOpaliteOres, 2));
     }
 
 
@@ -47,8 +49,8 @@ public class ModConfiguredFeatures {
         return ResourceKey.create(Registries.CONFIGURED_FEATURE, ResourceLocation.fromNamespaceAndPath(GemstoneMod.MOD_ID, name));
     }
 
-    private static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(BootstrapContext<ConfiguredFeature<?, ?>> context,
-                                                                                          ResourceKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {
+    private static <FC extends FeatureConfiguration, F extends  Feature<FC>> void register(BootstrapContext<ConfiguredFeature<?, ?>> context,
+                                                                                           ResourceKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {
         context.register(key, new ConfiguredFeature<>(feature, configuration));
     }
 }
